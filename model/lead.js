83f9163c8715b7d60a0ec7566cbeff127d250a8a
Francis Kojo Amekeh <franciskojoamekeh@gmail.com>
2025-05-31 23:29:40 +0000

    changed

--- model/lead.js
+++ model/lead.js
@@ -1,15 +1,17 @@
 import { Schema, model } from "mongoose";
 import mongoose from "mongoose";
 import { toJSON } from "@reis/mongoose-to-json";
+import { required } from "joi";
 
 const leadSchema = new Schema({
-    leadName: { type: String, required: true },
+    fullName: { type: String, required: true },
     email: { type: String, required: true, unique: true, lowercase: true },
     phone: { type: String, required: true },
     status: { type: String, default: 'new' },
     notes: { type: String },
     owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    updatedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+    updatedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    leadSource: {type: String, required: true}
 },
     {
         timestamps: true,
